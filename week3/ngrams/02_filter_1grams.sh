35bb5363225c016629758c6cc0401d69ccca744b
sbleier <sarabbleier@gmail.com>
2025-06-16 12:18:10 -0400

    1-3 on citibikes

--- week3/ngrams/02_filter_1grams.sh
+++ week3/ngrams/02_filter_1grams.sh
@@ -5,6 +5,7 @@
 gunzip googlebooks-eng-all-1gram-20120701-1.gz
 #   then filter out rows that match using grep -E, egrep, awk, or similar
  grep -E '[1-2][8,9,0][0-9][0-9]'  googlebooks-eng-all-1gram-20120701-1 > year_counts.tsv
+
 #   write results to year_counts.tsv
 
 
